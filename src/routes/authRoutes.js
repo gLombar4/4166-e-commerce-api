6788f72b5ca373b98e36dea253f21085ee1a27f0
gabejimenez22 <gabejimenez22@outlook.com>
2025-11-23 19:13:48 -0500

    file fixes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import { signUpHandler, logInHandler } from '../controllers/authController.js';
-import { validateUserUpdate, validateUser } from '../middleware/userValidators.js';
+import { validateSignup, validateUser } from '../middleware/userValidators.js';
 import logInLimiter from '../middleware/rateLimiter.js';
-
 
 const router = express.Router();
 
-router.post('/signup', validateUserUpdate, signUpHandler);
+router.post('/signup', validateSignup, signUpHandler);
 router.post('/login', logInLimiter, validateUser, logInHandler);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,31 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-
-import orderRoutes from './routes/orderRoutes.js';
-
+import orderRoutes from './routes/orderRoutes.js'
 import authRoutes from './routes/authRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
-
-import userRoutes from './routes/userRoutes.js';
-
 import productRoutes from './routes/productRoutes.js';
-
+import userRoutes from './routes/userRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.use(morgan('tiny'));
+
 app.use(express.json());
-
-
 app.use('/api/orders', orderRoutes);
-
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
-
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
@@ -36,6 +28,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   console.log(err.stack);
   if (!err.status) {
+    console.log(err.stack);
     err.status = 500;
     err.message = 'Internal Server Error';
   }
